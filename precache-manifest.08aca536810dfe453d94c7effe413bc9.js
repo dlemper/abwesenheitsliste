8fa2d96974f247b58317d0b1a55b372df14259a6
Daniel Lemper <ich@dlemper.de>
2020-08-30 21:35:40 +0200

    deploy

---
+++ precache-manifest.08aca536810dfe453d94c7effe413bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047058c7d03499963519",
+    "url": "/abwesenheitsliste/css/app.9e937d67.css"
+  },
+  {
+    "revision": "8e582ae1f2e13a8d5a95d96d398196b6",
+    "url": "/abwesenheitsliste/index.html"
+  },
+  {
+    "revision": "047058c7d03499963519",
+    "url": "/abwesenheitsliste/js/app.c545f962.js"
+  },
+  {
+    "revision": "a61d758de855c961d50e",
+    "url": "/abwesenheitsliste/js/chunk-vendors.efe04346.js"
+  },
+  {
+    "revision": "9082eafb7c73a6029378d69d6b6264b3",
+    "url": "/abwesenheitsliste/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/abwesenheitsliste/robots.txt"
+  }
+]);
